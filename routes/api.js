ac7aaf34dc9da5899197636dd21ee9030d57f543
Zmoore <zmoore371@gmail.com>
2021-12-10 14:53:34 -0500

    Getting data from db for workouts and creating totalDuration field that populates homepage

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,38 @@
 const router = require("express").Router();
 const db = require("../models");
-// const Workout = require("../models/workout.js")
-
-
-
 
 
 // get workouts
-router.get("/api/workouts", (req, res) => {
-    db.Workout.find({}, (err, data) => {
-        if(err) {
-            console.log(err)
-        } else {
-            res.json(data)
-            console.log("--------",)  
-        }
-    })
-})
+// router.get("/api/workouts", (req, res) => {
+//     db.Workout.find({}, (err, data) => {
+//         if(err) {
+//             console.log(err)
+//         } else {
+//             res.json(data)
+//             console.log("--------",)  
+//         }
+//     })
+// })
 
+router.get("/api/workouts", (req, res) => {
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
+            },
+        },
+    ])
+        .then((workout) => {
+            // console.log(workout);
+            res.json(workout)
+        })
+        .catch((e) => {
+            res.json(e)
+        })
+    
+});
 
 
 
